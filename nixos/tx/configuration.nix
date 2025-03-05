31e521f93d1c5d7747eb70916577f5ca1249d507
qhj <me@qhj.moe>
2025-03-05 22:48:03 +0800

    qemu hooks

--- nixos/tx/configuration.nix
+++ nixos/tx/configuration.nix
@@ -232,6 +232,23 @@
           ]
         '';
       };
+      hooks.qemu = {
+        isolcpus-hook = pkgs.writers.writeBash "isolcpus" ''
+          #!/bin/sh
+
+          command=$2
+
+          if [ "$command" = "started" ]; then
+              systemctl set-property --runtime -- system.slice AllowedCPUs=4-15
+              systemctl set-property --runtime -- user.slice AllowedCPUs=4-15
+              systemctl set-property --runtime -- init.scope AllowedCPUs=4-15
+          elif [ "$command" = "release" ]; then
+              systemctl set-property --runtime -- system.slice AllowedCPUs=0-23
+              systemctl set-property --runtime -- user.slice AllowedCPUs=0-23
+              systemctl set-property --runtime -- init.scope AllowedCPUs=0-23
+          fi
+        '';
+      };
     };
   };
   programs.virt-manager.enable = true;
