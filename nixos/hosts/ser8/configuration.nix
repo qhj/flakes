0f8b08e9f259d096b9876b7956e59838d1be99f0
qhj <me@qhj.moe>
2026-02-13 14:00:37 +0800

    ser8: add firefox preferences

--- nixos/hosts/ser8/configuration.nix
+++ nixos/hosts/ser8/configuration.nix
@@ -131,6 +131,10 @@ in
     enable = true;
   };
   programs.firefox.enable = true;
+  programs.firefox.preferences = {
+    "browser.tabs.inTitlebar" = 0;
+    "ui.key.menuAccessKeyFocuses" = false;
+  };
   users = {
     groups.qhj.gid = 1000;
     users.qhj = {
@@ -151,7 +155,7 @@ in
     type = "fcitx5";
     fcitx5 = {
       addons = with pkgs; [
-        fcitx5-chinese-addons
+        qt6Packages.fcitx5-chinese-addons
       ];
       waylandFrontend = true;
     };
