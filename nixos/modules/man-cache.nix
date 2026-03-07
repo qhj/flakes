a3e0e12fc6abbf7f47e62a10e3e47120c22e7a31
qhj <me@qhj.moe>
2026-03-07 19:23:40 +0800

    nix flake update

--- nixos/modules/man-cache.nix
+++ nixos/modules/man-cache.nix
@@ -30,7 +30,7 @@ lib.mkMerge [
     lib.mkIf (cfg.enable) {
 
       # We generate the cache ourselves.
-      documentation.man.generateCaches = false;
+      documentation.man.cache.enable = false;
 
       systemd.services."man-db" = {
         requires = [ "sysinit-reactivation.target" ];
