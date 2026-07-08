13157eee25d6ec67c0a1966c345e7594d6bd28d7
qhj <me@qhj.moe>
2026-07-08 22:28:53 +0800

    update ser8 config

--- hosts/ser8/configuration.nix
+++ hosts/ser8/configuration.nix
@@ -189,74 +189,18 @@
     defaultSopsFile = ../../ser8.yaml;
     age.keyFile = "/var/lib/sops-nix/key.txt";
   };
-  sops.secrets."dae/subscription" = {
-    mode = "0400";
-  };
-  sops.templates.dae-config = {
-    content = ''
-      global {
-        # Bind to LAN and/or WAN as you want. Replace the interface name to your own.
-        lan_interface: br0
-        wan_interface: auto # Use "auto" to auto detect WAN interface.
-
-        log_level: info
-        allow_insecure: false
-        auto_config_kernel_parameter: true
-      }
-
-      subscription {
-        "${config.sops.placeholder."dae/subscription"}"
-      }
-
-      # See https://github.com/daeuniverse/dae/blob/main/docs/en/configuration/dns.md for full examples.
-      dns {
-        upstream {
-          googledns: 'tcp+udp://dns.google:53'
-          alidns: 'udp://223.5.5.5:53'
-        }
-        routing {
-          request {
-            qtype(https) -> reject
-            fallback: alidns
-          }
-          response {
-            upstream(googledns) -> accept
-            ip(geoip:private) && !qname(geosite:cn) -> googledns
-            fallback: accept
-          }
-        }
-      }
-
-      group {
-        proxy {
-          #filter: name(keyword: HK, keyword: SG)
-          policy: min_moving_avg
-        }
-      }
-
-      # See https://github.com/daeuniverse/dae/blob/main/docs/en/configuration/routing.md for full examples.
-      routing {
-        pname(NetworkManager) -> direct
-        dip(224.0.0.0/3, 'ff00::/8') -> direct
-
-        ### Write your rules below.
-
-        # Disable h3 because it usually consumes too much cpu/mem resources.
-        l4proto(udp) && dport(443) -> block
-        dip(geoip:private) -> direct
-        dip(geoip:cn) -> direct
-        domain(geosite:cn) -> direct
-
-        fallback: proxy
-      }
-    '';
-    mode = "0400";
-  };
-  services.dae = {
-    enable = true;
-    configFile = config.sops.templates.dae-config.path;
-  };
   services.udev.packages = with pkgs; [ canokeys-udev-rules ];
-  programs.ssh.startAgent = true;
+  programs.ssh = {
+    startAgent = true;
+    extraConfig = ''
+      Host *
+        SetEnv TERM=xterm-256color
+      Host 192.168.77.1
+        ForwardAgent yes
+      Host github.com
+        Hostname ssh.github.com
+        Port 443
+    '';
+  };
   services.netbird.enable = true;
 }
