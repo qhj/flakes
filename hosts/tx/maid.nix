83c42ad7969392d26cb466aeeac505a432253931
qhj <me@qhj.moe>
2026-09-11 02:43:26 +0800

    tx: configure input method hotkeys

--- hosts/tx/maid.nix
+++ hosts/tx/maid.nix
@@ -31,9 +31,21 @@
       patch:
         __include: rime_ice_suggestion:/
         menu/page_size: 7
+        ascii_composer/switch_key/Shift_L: commit_code
 
         schema_list:
           - schema: rime_ice
+    '';
+
+    file.xdg_config."fcitx5/config".text = ''
+      [Hotkey/TriggerKeys]
+      0=Control+Shift+Shift_R
+
+      [Hotkey/AltTriggerKeys]
+      0=Shift+Shift_R
+
+      [Behavior]
+      ActiveByDefault=True
     '';
   };
 }
